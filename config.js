55c57532bb0f94dc73ffb124ca02c6bf9cd65421
DragonHeartYT <galacticmoonyt24@gmail.com>
2026-06-15 16:27:31 -0800

    Update config.js

--- config.js
+++ config.js
@@ -36,15 +36,16 @@ nodes: [
         name: "TriniumHost"
     }
 ],
-    nodes: [
-        {
-            host: "localhost",
-            password: "youshallnotpass",
-            port: 443,
-            secure: false,
-            name: "Main"
-        }
-    ],
 
     defaultSearchPlatform: "ytmsearch",
     restVersion: "v4",
+
+    accentColor: 0x2b2d31,
+
+    // Musicard theme config
+    musicard: {
+        theme: "Bloom",
+        progressBarColor: "#FACC15",
+        backgroundColor: "#2b2d31",
+    },
+};
