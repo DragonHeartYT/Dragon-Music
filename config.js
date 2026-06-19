cb54a5c68d2b93fd94bfb18430db5b15327bc20a
DragonHeartYT <galacticmoonyt24@gmail.com>
2026-06-19 12:42:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,51 @@
 module.exports = {
-    nodes: [
-        {
-            Host: lavalinkv4.serenetia.com
-            Port: 443
-            Password: "https://seretia.link/discord"
-            Secure: true,
-            name: "Main"
-        }
-    ],
+nodes: [
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/myUcUguxbQ",
+        port: 443,
+        secure: true,
+        name: "Main"
+    },
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/myUcUguxbQ",
+        port: 443,
+        secure: true,
+        name: "Serenetia"
+    },
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/myUcUguxbQ",
+        port: 443,
+        secure: true,
+        name: "Jirayu"
+    },
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/myUcUguxbQ",
+        port: 443,
+        secure: true,
+        name: "MilloHost"
+    },
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/myUcUguxbQ",
+        port: 443,
+        secure: true,
+        name: "TriniumHost"
+    }
+],
+
+    defaultSearchPlatform: "ytmsearch",
+    restVersion: "v4",
+
+    accentColor: 0x2b2d31,
+
+    // Musicard theme config
+    musicard: {
+        theme: "Bloom",
+        progressBarColor: "#FACC15",
+        backgroundColor: "#2b2d31",
+    },
+};
