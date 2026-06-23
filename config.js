c0326c1afea0148c5101b9fc65a0142d8faae5c5
DragonHeartYT <galacticmoonyt24@gmail.com>
2026-06-23 10:02:18 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,10 @@
 module.exports = {
     nodes: [
         {
-            Host: lavalinkv4.serenetia.com
+            Host: lavalink-v4.triniumhost.com
             Port: 443
             Password: "https://discord.gg/CxsBjs5RAU"
             Secure: true,
             name: "Main"
         }
     ],
-
-    defaultSearchPlatform: "ytmsearch",
-    restVersion: "v4",
-
-    accentColor: 0x2b2d31,
-
-    // Musicard theme config
-    musicard: {
-        theme: "Bloom",
-        progressBarColor: "#FACC15",
-        backgroundColor: "#2b2d31",
-    },
-};
