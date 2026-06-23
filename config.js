d9fd1be4d369e9ad179975a75173adbe1aa39a11
DragonHeartYT <galacticmoonyt24@gmail.com>
2026-06-23 10:05:58 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,51 @@
 module.exports = {
-    nodes: [
-        {
-            Host: lavalink-v4.triniumhost.com
-            Port: 443
-            Password: "https://discord.gg/CxsBjs5RAU"
-            Secure: true,
-            name: "Main"
-        }
-    ],
+nodes: [
+    {
+        host: "lavalink-v4.triniumhost.com",
+        password: "https://discord.gg/CxsBjs5RAU",
+        port: 443,
+        secure: true,
+        name: "Main"
+    },
+    {
+        host: "lava-v4.millohost.my.id",
+        password: "https://discord.gg/CxsBjs5RAU",
+        port: 443,
+        secure: true,
+        name: "Serenetia"
+    },
+    {
+        host: "lavalink.jirayu.net",
+        password: "https://discord.gg/CxsBjs5RAU",
+        port: 443,
+        secure: true,
+        name: "Jirayu"
+    },
+    {
+        host: "lavalinkv4.serenetia.com",
+        password: "https://discord.gg/CxsBjs5RAU",
+        port: 443,
+        secure: true,
+        name: "MilloHost"
+    },
+    {
+        host: "lavalink-v4.triniumhost.com",
+        password: "https://discord.gg/CxsBjs5RAU",
+        port: 443,
+        secure: true,
+        name: "TriniumHost"
+    }
+],
+
+    defaultSearchPlatform: "ytmsearch",
+    restVersion: "v4",
+
+    accentColor: 0x2b2d31,
+
+    // Musicard theme config
+    musicard: {
+        theme: "Bloom",
+        progressBarColor: "#FACC15",
+        backgroundColor: "#2b2d31",
+    },
+};
